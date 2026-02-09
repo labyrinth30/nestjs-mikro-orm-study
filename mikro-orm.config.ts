16fd9d8367117f8debd9fc0b77aac91d7e0c5421
younha00 <ayeon0@office.skhu.ac.kr>
2026-02-09 17:27:48 +0900

    Fix: PostgreSQL driver 타입 해결

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -1,15 +1,13 @@
-import { Options } from '@mikro-orm/core';
-import { PostgreSqlDriver } from '@mikro-orm/postgresql';
+import { defineConfig } from '@mikro-orm/postgresql'; // 핵심 변경 사항
 import { config } from 'dotenv';
 
 config();
 
-const mikroOrmConfig: Options = {
-  driver: PostgreSqlDriver,
-  // 기존 개별 설정 대신 clientUrl 사용
+export default defineConfig({
+
   clientUrl: process.env.POSTGRES_URL,
 
-  // Vercel(Neon) DB 연결을 위한 SSL 설정 (필수)
+  // Vercel(Neon) DB 연결을 위한 SSL 설정
   driverOptions: {
     connection: {
       ssl: {
@@ -25,6 +23,4 @@ const mikroOrmConfig: Options = {
     path: 'dist/migrations',
     pathTs: 'src/migrations',
   },
-};
-
-export default mikroOrmConfig;
+});
